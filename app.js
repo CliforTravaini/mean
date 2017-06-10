8e085b6aac8225ad8f7536007a35d002ff39f940
CliforTravaini <clifortravaini@gmail.com>
2017-06-10 12:06:27 -0300

    aula 3

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-const TesteAPI = require('./api/routes/testeRoutes');
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1/mean')
 
+const TesteAPI = require('./api/routes/testeRoutes')
+const UsuarioAPI = require('./api/routes/UsuarioRoutes')
 
 
 //var index = require('./routes/index');
@@ -26,8 +29,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/teste', TesteAPI);
-
+app.use('/teste', TesteAPI)
+app.use('/api/v1/usuario', UsuarioAPI)
 
 //app.use('/', index);
 //app.use('/users', users);
